73e7cb878561f302fac212499fb91b3ffa671ce2
Amal Gunatilake <daag123@gmail.com>
2024-01-20 03:26:45 +1100

    Add, Update, Edit Notes

--- src/app/notes/NavBar.tsx
+++ src/app/notes/NavBar.tsx
@@ -1,31 +1,49 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import logo from "@/assets/logo.png";
 import { UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
+import { useState } from "react";
+import AddEditNoteDialog from "@/components/AddEditNoteDialog";
+import ThemeToggleButton from "@/components/ThemeToggleButton";
+import { dark } from "@clerk/themes";
+import { useTheme } from "next-themes";
 
 export default function NavBar() {
+  const { theme } = useTheme();
+  const [showAddEditNoteDialog, setShowAddEditNoteDialog] = useState(false);
+
   return (
-    <div className="p-4 shadow">
-      <div className="m-auto flex max-w-7xl flex-wrap items-center justify-between gap-3">
-        <Link href="/notes" className="flex items-center gap-1">
-          <Image alt="Ai Mentor logo" src={logo} width={40} height={40} />
-          <span className="font-bold">Ai Mentor</span>
-        </Link>
-        <div className="flex items-center gap-2">
-          <UserButton
-            afterSignOutUrl="/"
-            appearance={{
-              elements: { avatarBox: { width: "2.5rem", height: "2.5rem" } },
-            }}
-          />
-          <Button>
-            <Plus size={20} className="mr-2" />
-            Add Note
-          </Button>
+    <>
+      <div className="p-4 shadow">
+        <div className="m-auto flex max-w-7xl flex-wrap items-center justify-between gap-3">
+          <Link href="/notes" className="flex items-center gap-1">
+            <Image alt="Ai Mentor logo" src={logo} width={40} height={40} />
+            <span className="font-bold">Ai Mentor</span>
+          </Link>
+          <div className="flex items-center gap-2">
+            <UserButton
+              afterSignOutUrl="/"
+              appearance={{
+                baseTheme: theme === "dark" ? dark : undefined,
+                elements: { avatarBox: { width: "2.5rem", height: "2.5rem" } },
+              }}
+            />
+            <ThemeToggleButton />
+            <Button onClick={() => setShowAddEditNoteDialog(true)}>
+              <Plus size={20} className="mr-2" />
+              Add Note
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
+      <AddEditNoteDialog
+        open={showAddEditNoteDialog}
+        setOpen={setShowAddEditNoteDialog}
+      />
+    </>
   );
 }
---
+++ src/components/Note.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { Note as NoteModel } from "@prisma/client";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { create } from "domain";
+import { useState } from "react";
+import AddEditNoteDialog from "./AddEditNoteDialog";
+
+interface NoteProps {
+  note: NoteModel;
+}
+
+export default function Note({ note }: NoteProps) {
+  const [showEditNoteDialog, setShowEditNoteDialog] = useState(false);
+
+  const wasUpdated = note.updatedAt > note.createdAt;
+
+  const createdUpdatedAtTimestamp = (
+    wasUpdated ? note.updatedAt : note.createdAt
+  ).toDateString();
+
+  return (
+    <>
+      <Card
+        className="cursor-pointer transition-shadow hover:shadow-lg"
+        onClick={() => setShowEditNoteDialog(true)}
+      >
+        <CardHeader>
+          <CardTitle>{note.title}</CardTitle>
+          <CardDescription>
+            {createdUpdatedAtTimestamp}
+            {wasUpdated && " (updated)"}
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p className="whitespace-pre-line">{note.content}</p>
+        </CardContent>
+      </Card>
+      <AddEditNoteDialog
+        open={showEditNoteDialog}
+        setOpen={setShowEditNoteDialog}
+        noteToEdit={note}
+      />
+    </>
+  );
+}
