67f106589c36fd86ccbf35a0345de62b75a49853
Amal Gunatilake <daag123@gmail.com>
2024-01-19 20:22:35 +1100

    Authentication with Clerk

---
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,14 @@
+import { SignUp } from "@clerk/nextjs";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Ai Mentor - Sign Up",
+};
+
+export default function SignUpPage() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <SignUp appearance={{ variables: { colorPrimary: "#16A249" } }} />
+    </div>
+  );
+}
